edeaec50e2ba2abdde283b72964a73637dda6bf0
RabeyaAS <rabeya.98@hotmail.com>
2024-10-03 17:58:22 +0100

    added partially the money event handler

--- js/home.js
+++ js/home.js
@@ -1,1 +1,25 @@
-console.log('home added')
+// add money to the account
+/**
+ * Step-1 : add an event handler to the add money button inside the form
+ *    1.1 : prevent page reload after form submit
+ * Step-2 : get money to be added to the account balance
+ * 
+ */
+
+// Step-1 : add an event handler to the add money button inside the form
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    // prevent page reload after form submit
+    event.preventDefault();
+
+    // Step-2 : get money to be added to the account balance
+    const addMoneyInput = document.getElementById('input-add-money');
+    console.log(addMoneyInput.value);
+
+    // get the pin number provided
+    const pinNumber = document.getElementById('input-pin-number');
+    console.log(pinNumber.value);
+
+
+
+})
+
