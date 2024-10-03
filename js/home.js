be98459af8bd4ef744eb731dc7146dfed3f99f26
RabeyaAS <rabeya.98@hotmail.com>
2024-10-03 18:47:45 +0100

    added money calculation and updated to the current balance complete

--- js/home.js
+++ js/home.js
@@ -3,6 +3,12 @@
  * Step-1 : add an event handler to the add money button inside the form
  *    1.1 : prevent page reload after form submit
  * Step-2 : get money to be added to the account balance
+ *    2.2 : get the pin number
+ * Step 3 : Verify the pin number
+ * 
+ * Make sure the money value type is in number when calculating if its a string then it
+ * won't calculate. So to convert a string into a number you use parseFloat --> incase
+ * the result has decimals then it will include them too.
  * 
  */
 
@@ -12,13 +18,34 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
     event.preventDefault();
 
     // Step-2 : get money to be added to the account balance
-    const addMoneyInput = document.getElementById('input-add-money');
-    console.log(addMoneyInput.value);
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput);
 
     // get the pin number provided
-    const pinNumber = document.getElementById('input-pin-number');
-    console.log(pinNumber.value);
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+    console.log(pinNumberInput);
 
+    // Step-3 ; verify pin number 
+    // wrong way to validate pin number
+    if(pinNumberInput === '1234'){
+        console.log('adding money to your account')
+
+        // Step-4 : get the current balance
+        const balance = document.getElementById('account-balance').innerText;
+        console.log(typeof balance);
+
+        // Step-5 : add addMoneyInput with balance
+        const addMoneyNumber = parseFloat(addMoneyInput);
+        const balanceNumber = parseFloat(balance);
+        const newBalance = addMoneyNumber + balanceNumber;
+        console.log(newBalance);
+
+        // Step-6 : update the balance in the UI/DOM
+        document.getElementById('account-balance').innerText = newBalance;
+    }
+    else{
+        alert('Failed to add money! Please try again')
+    }
 
 
 })
